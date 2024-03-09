c49c01cf30e15c904d68c8807ce097c236892383
ahmadkhalidchaudhary@gmail.com <mianabubaker007@gmail.com>
2024-03-09 19:04:53 +0500

    abc

--- test.cpp
+++ test.cpp
@@ -2,32 +2,6 @@
 using namespace std;
 
 int main() {
-
-    int a;
-    cout << "Enter the first number : " << endl;
-    cin >> a;
-    int b;
-    cout << " Enter the second number : " << endl;
-    cin >> b;
-
-    cout << "Enter the opertor :" << endl;
-    char ch;
-    cin >> ch;
-    switch (ch) {
-    case'+':
-        cout << "sum of two number is :" << a + b << endl;
-        break;
-    case'-':
-        cout << "the subtraction a-b : " << a - b << endl;
-        break;
-    case'*':
-        cout << "the multiplication of  a*b : " << a * b << endl;
-        break;
-    case'/':
-        cout << "the division of  a/b : " << a / b << endl;
-        break;
-    default:
-        cout << "this case is deafault case " << endl;
-    }
+     
 
 }
